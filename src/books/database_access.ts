075450120ca6434f2a4245fc49b024169b0be850
vivian55636 <okpalau@mcmaster.ca>
2025-09-02 10:24:37 -0400

    iniial commit

--- src/books/database_access.ts
+++ src/books/database_access.ts
@@ -1,12 +1,42 @@
-import { MongoClient } from 'mongodb'
+import { type Collection, type Db, MongoClient } from 'mongodb'
 // We are importing the book type here, so we can keep our types consistent with the front end
-import { type Book } from '../../schema/type'
+import { type Book } from '../documented_types'
 
 // This is the connection string for the mongo database in our docker compose file
-const uri = 'mongodb://localhost:27017'
+// We're using process.env to detect if a different mongo uri is set, primarily for testing purpuses
+const uri = (global as any).MONGO_URI as string ?? 'mongodb://mongo-books'
 
 // We're setting up a client, opening the database for our project, and then opening
 // a typed collection for our books.
 export const client = new MongoClient(uri)
-export const database = client.db('mcmasterful-books')
-export const bookCollection = database.collection<Book>('books')
+
+// We're moving the setup of the database and collections into a function with a returned value,
+// to allow us to isolate them in tests
+
+export interface BookDatabaseAccessor {
+  database: Db
+  books: Collection<Book>
+}
+
+export interface AppBookDatabaseState {
+  books: BookDatabaseAccessor
+}
+
+export function getBookDatabase (dbName?: string): BookDatabaseAccessor {
+  const database = client.db(dbName ?? Math.floor(Math.random() * 100000).toPrecision().toString())
+  const books = database.collection<Book>('books')
+
+  return {
+    database,
+    books
+  }
+}
+
+if (import.meta.vitest !== undefined) {
+  const { test, expect } = import.meta.vitest
+
+  test('Can Setup Test DB', () => {
+    const { database } = getBookDatabase()
+    expect(database.databaseName, `URI: ${uri}, DB: ${database.databaseName}`).not.toEqual('mcmasterful-books')
+  })
+}
